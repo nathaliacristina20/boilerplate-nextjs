3ee2bf67f69e12594746b95b2d0e9687f190e9c4
Nathalia Cristina Gomes Oliveira <nathaliacristina20@hotmail.com>
2020-07-05 22:24:15 -0300

    chore: add storybook addon knobs

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,9 @@
 module.exports = {
   stories: ['../src/components/**/stories.tsx'],
-  addons: ['@storybook/preset-typescript', '@storybook/addon-actions', '@storybook/addon-links'],
+  addons: [
+    '@storybook/preset-typescript',
+    '@storybook/addon-actions',
+    '@storybook/addon-links',
+    '@storybook/addon-knobs/register'
+  ],
 };
--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,15 +1,16 @@
 import * as Style from './styles';
 
-const Main = () => (
+const Main = ({
+  title = 'React Avançado',
+  description = 'Typescript, ReactJS, NextJS e Styled Components'
+}) => (
   <Style.Wrapper>
     <Style.Logo
       src="/img/logo.svg"
       alt="Imagem de um atomo e React Avancado escrito ao lado"
     />
-    <Style.Title>React Avançado</Style.Title>
-    <Style.Description>
-      Typescript, ReactJS, NextJS e Styled Components
-    </Style.Description>
+    <Style.Title>{title}</Style.Title>
+    <Style.Description>{description}</Style.Description>
     <Style.Illustration
       src="/img/hero-illustration.svg"
       alt="Um desenvolvedor de frente para a tela com codigo"
