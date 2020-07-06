507f6a2621d8322e8c68907d0b4ed7eae5cea764
Nathalia Cristina Gomes Oliveira <nathaliacristina20@hotmail.com>
2020-07-05 21:07:38 -0300

    feature: styling index

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,20 @@
+import * as Style from './styles';
+
 const Main = () => (
-  <main>
-    <h1>React Avancado</h1>
-  </main>
+  <Style.Wrapper>
+    <Style.Logo
+      src="/img/logo.svg"
+      alt="Imagem de um atomo e React Avancado escrito ao lado"
+    />
+    <Style.Title>React Avançado</Style.Title>
+    <Style.Description>
+      Typescript, ReactJS, NextJS e Styled Components
+    </Style.Description>
+    <Style.Illustration
+      src="/img/hero-illustration.svg"
+      alt="Um desenvolvedor de frente para a tela com codigo"
+    />
+  </Style.Wrapper>
 );
 
 export default Main;
