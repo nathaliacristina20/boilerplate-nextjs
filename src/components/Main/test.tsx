66207881eec521625712d32084d6f1bda6091258
Nathalia Cristina Gomes Oliveira <nathaliacristina20@hotmail.com>
2020-07-05 21:09:21 -0300

    refactor: fix tests render heading

--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -7,9 +7,15 @@ describe('<Main />', () => {
     const { container } = render(<Main />);
 
     expect(
-      screen.getByRole('heading', { name: /react avancado/i })
+      screen.getByRole('heading', { name: /react avançado/i })
     ).toBeInTheDocument();
 
     expect(container.firstChild).toMatchSnapshot();
+  });
+
+  it('should render the colors correctly', () => {
+    const { container } = render(<Main />);
+
+    expect(container.firstChild).toHaveStyle({ 'background-color': '#06092b' });
   });
 });
